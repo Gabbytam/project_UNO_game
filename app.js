0c73593482834636bf950759587a1587a72eb8c6
Gabbytam <gtam2@fordham.edu>
2020-09-25 01:21:32 -0700

    added startCard function that follows the rules of uno

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-console.log('(uno) what it isssss');
-
-let cardType= ['red', 'yellow', 'green', 'blue'];
-let cardValue= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'switch', 'reverse'];
-let specialCardValue= ['wild Card', 'Wild Card +4'];
+console.log('(u-no) what it isssss');
+//variables for all the card types and values 
+const cardType= ['red', 'yellow', 'green', 'blue'];
+const cardValue= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'switch', 'reverse', '+2'];
+const specialCardValue= ['Wild Card', 'Wild Card +4'];
+let drawPile;
 
 //MIGHT BE BETTER TO USE CLASS SO YOU CAN ACCESS TYPE AND VALUE 
-
-//first made a class Card that takes in 2 values
+//first made a class Card that takes in 2 attributes, the type and value 
 class Card {
     constructor(type, value){
         this.type= type;
         this.value= value;
     }
 }
-
 //then made a factory that connects the Card class within its method, it also provides arrays that represent decks
 class Factory {
     constructor() {
@@ -49,12 +48,55 @@ class Factory {
     }
 }
 
+//HERE IS WHERE WE USE/CALL OUR CLASSES AND CALL THEIR METHODS
 const newDeck= new Factory(); //have to instantiate(?) using Factory and set it to variable deck 
 newDeck.makeDeck(); //call the makeDeck function on deck
 // const allCards= newDeck.deckOfCards;
 // console.log('this is all cards', allCards);
 newDeck.shuffleDeck(newDeck.deckOfCards);//call shuffleDeck method on 
 console.log(newDeck); //now we see that deck contains a filled deckOfCards array
+
+//FUNCTION THAT DEALS OUT THE CARDS 
+//use this as a callback function for an event listener 
+const dealCards= ()=> {
+    //initialize empty arrays for the player's cards 
+    let player1Cards= [];
+    let player2Cards=[];
+    //each player gets 7 cards to start 
+    for(let i=0; i<14; i++){ //loop 14 times 
+        if(i%2== 0){ //check if i is even number. Way of switching off cards when dealing to 2 players
+            player1Cards.push(newDeck.shuffledCards[i]); //add card to array for Player1
+        } else if(i%2== 1){ //check if i is odd number
+            player2Cards.push(newDeck.shuffledCards[i]); //add card to array for Player2
+        }
+        newDeck.shuffledCards.shift(); //removes the card from beginning of array, resembles dealing them out, which would take the dealed out cards away from the shuffled deck 
+    }
+    drawPile= newDeck.shuffledCards;
+    console.log('cards for player 1', player1Cards);
+    console.log('cards for player 2', player2Cards);
+    console.log('this is the remaining deck', newDeck.shuffledCards); //same as below
+    console.log('this is the drawPile', drawPile); //same as above
+}
+dealCards();//call function to get it to run //MAYBE USE AN EVENT LISTENER BUTTON TO DEAL CARD, LIKE START GAME
+
+//FUNCTION THAT WILL PUT TOP CARD DOWN
+const startCard= ()=> {
+    //we're using drawPile to make the code make more sense, but we could also just use newDeck.shuffledCards because that value as it continues through code updates 
+   
+    //rule is that if top card is one of the wild cards, draw another card as the start card 
+    if(drawPile[0].type.includes('Wild')){ //checks the type of the first card in the drawPile to see if it includes the string, Wild, and if so:
+        console.log('That cant be your start card...sorry');
+        drawPile.push(drawPile[0]); //first, move that card to the end of the array so it can be used later
+        drawPile.shift(); //second, remove it from the beginning of the drawPile array
+        topCard=drawPile[0]; //third, set the top card to the NEW first card in the drawPile array
+    } else { //for any card that isnt type 'Wild'
+        topCard=drawPile[0];
+    }
+    console.log('this is the starting/top card', topCard);
+}
+startCard();//invoke the function //WILL ALSO PROBABLY BE PASSED INTO AN EVENT LISTENER 
+
+//FUNCTION THAT ALLOWS PLAYER TO DRAW CARD
 
 
 document.addEventListener('DOMContentLoaded', ()=> {
